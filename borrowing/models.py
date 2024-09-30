9176389102f6ae7091a197267b1a4c68f2f788d2
Tetyana Pavlyuk <tetyana.pas@gmail.com>
2024-09-30 14:49:43 +0300

    Create borrowing model

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+from book.models import Book
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField()
+    book = models.ManyToManyField(Book)
+    user = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name="borrowings"
+    )
+
+    def __str__(self):
+        return f"{self.user.name}: {self.book.name}"
+
+    class Meta:
+        ordering = ["-borrow_date"]
