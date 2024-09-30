51c6f4793082dd2f27fe97b57e76186af58b6813
Tetyana Pavlyuk <tetyana.pas@gmail.com>
2024-09-30 14:17:06 +0300

    Create AdminOrReadOnly permission for books and used JWT authentification

--- book/views.py
+++ book/views.py
@@ -1,7 +1,9 @@
 from rest_framework import mixins
 from rest_framework.viewsets import GenericViewSet
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from book.models import Book
+from book.permissions import AdminOrReadOnly
 from book.serializers import BookSerializer
 
 
@@ -15,3 +17,5 @@ class BookViewSet(
 ):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    authentication_classes = (JWTAuthentication, )
+    permission_classes = (AdminOrReadOnly, )
